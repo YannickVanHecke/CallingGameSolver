72110ae78a7138820690e7fd69c1f96c9a688bf5
Yannick <yannick.vanhecke@gmail.com>
2024-01-18 16:31:10 +0100

    added dependency injection for CallGameService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CallGameService } from '../services/CallGameService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [CallGameService]
 })
 export class AppComponent {
   title = 'Los het belspel op';
@@ -13,12 +15,12 @@ export class AppComponent {
   /**
    *
    */
-  constructor() {
+  constructor(private callGameService: CallGameService) {
 
     this.Assignment = "VANNACHT WEER" + "<br/>EEN WINNAAR!<br/><br/>DRIEëndertig gedeeld door 3 + 199 - 9 + TIEN";
   }
 
   public solve() {
-
+    
   }
 }
