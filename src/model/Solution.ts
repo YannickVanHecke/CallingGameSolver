1c15ae93cf2959e02413f768b9131f4c1bd431d3
Yannick <yannick.vanhecke@gmail.com>
2024-02-06 19:40:29 +0100

    adding global result of solution.

--- src/model/Solution.ts
+++ src/model/Solution.ts
@@ -2,7 +2,7 @@ import { Step } from "./Step";
 
 export class Solution {
     public Steps: Array<Step>;
-
+    public Result: Number = 0;
     constructor() {
         this.Steps = new Array<Step>();
     }
